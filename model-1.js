69450d127b6357af0616820308424a9ed5ff66af
Anna Kummer <annamkummer@gmail.com>
2021-07-19 14:38:14 -0600

    Add booleans

--- model-1.js
+++ model-1.js
@@ -5,3 +5,7 @@ var potionsMaster = "Snape";
 var numberOfYears = 7;
 var ageToEnroll = 11;
 var numPetsAllowed = 1;
+
+var firstYearsAllowedBrooms = false;
+var studentsAllowedInForest = false;
+var firstYearsOnQuidditch = true;
