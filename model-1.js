f27a43a973c03029a25d8a9939252a27e6d46387
Anna Kummer <annamkummer@gmail.com>
2021-07-19 14:42:01 -0600

    Add arrays

--- model-1.js
+++ model-1.js
@@ -9,3 +9,7 @@ var numPetsAllowed = 1;
 var firstYearsAllowedBrooms = false;
 var studentsAllowedInForest = false;
 var firstYearsOnQuidditch = true;
+
+var petsAllowed = ["owl", "toad", "cat"];
+var quidditchBalls = ["Snitch", "Quaffle", "Bludgers"];
+var quidditchPlayers = ["Seeker", "Beaters", "Chasers", "Keeper"];
