f2b943fc8fbb5a0c420954428a51652c1ca42c15
Anna Kummer <annamkummer@gmail.com>
2021-07-19 14:46:05 -0600

    Add objects

--- model-1.js
+++ model-1.js
@@ -13,3 +13,31 @@ var firstYearsOnQuidditch = true;
 var petsAllowed = ["owl", "toad", "cat"];
 var quidditchBalls = ["Snitch", "Quaffle", "Bludgers"];
 var quidditchPlayers = ["Seeker", "Beaters", "Chasers", "Keeper"];
+
+var Gryffindor = {
+  colors: "Red and Gold",
+  animal: "Lion",
+  head: "McGonagall",
+  ghost: "Sir Nicolas"
+}
+
+var Ravenclaw = {
+  colors: "Blue and Silver",
+  animal: "Eagle",
+  head: "Flitwick",
+  ghost: "Gray Lady"
+}
+
+var Hufflepuff = {
+  colors: "Black and Yellow",
+  animal: "Badger",
+  head: "Sprout",
+  ghost: "Fat Friar"
+}
+
+var Slytherin = {
+  colors: "Green and Silver",
+  animal: "Snake",
+  head: "Snape",
+  ghost: "Bloody Baron"
+}
